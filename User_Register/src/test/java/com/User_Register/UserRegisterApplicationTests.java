c881672ef08bb3b80a1e90d5ce34ac5d90fc120b
sushma-sheshita-dulam <ssdulam@gmail.com>
2024-11-24 12:31:21 +0530

    Files added

---
+++ User_Register/src/test/java/com/User_Register/UserRegisterApplicationTests.java
@@ -0,0 +1,15 @@
+
+package com.UserRegister.User_Register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
